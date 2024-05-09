8afaff82d728a71d6b25832d00d0a0ad6c261fe2
kapil <kapilchaudhary8280@gmail.com>
2024-05-09 09:32:21 +0530

    added drive notes

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,12 @@
+export default function SubjectPage() {
+    return (
+        <div>
+        <iframe
+        allowTransparency={true}
+        src="https://drive.google.com/embeddedfolderview?id=1NCbi3-XX-CkIDlALogThytZzjXIdjl1L"
+        className="w-full h-screen border-0 bg-[#020817]"
+        allowFullScreen
+        ></iframe>
+        </div>
+    )
+};
