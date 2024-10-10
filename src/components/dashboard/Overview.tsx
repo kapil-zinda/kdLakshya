70a9337f4ddfcd3ae1169907d4b274894a1fde52
Rishabh Gaud <rishabh.kumbh@gmail.com>
2024-10-10 09:38:37 +0530

    nothing

--- src/components/dashboard/Overview.tsx
+++ src/components/dashboard/Overview.tsx
@@ -1,129 +1,226 @@
-"use client"
-import styled from "@emotion/styled";
-import { Button } from "@mui/material";
-import { PenIcon, User2Icon } from "lucide-react";
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
+import { Button } from '@mui/material';
+import axios from 'axios';
+import { PenIcon, User2Icon } from 'lucide-react';
 
 const Card = styled.div`
-    background: "#d3ebffcc";
-    position: relative;
-    cursor: pointer;
-    box-sizing: border-box;
-    -webkit-tap-highlight-color: transparent;
-    width: 100%;
-    padding: 0.8rem 1rem;
-    margin-top: 2rem;
-    border-radius: 0.5rem;
-    box-shadow: rgb(23 93 207 / 20%) 0px 2px 8px 0px, 0px 0px 2px #1949a1;
-    border: 1px solid rgb(66, 133, 244);
-    transition: 250ms;
+  background: '#d3ebffcc';
+  position: relative;
+  cursor: pointer;
+  box-sizing: border-box;
+  -webkit-tap-highlight-color: transparent;
+  width: 100%;
+  padding: 0.8rem 1rem;
+  margin-top: 2rem;
+  border-radius: 0.5rem;
+  box-shadow:
+    rgb(23 93 207 / 20%) 0px 2px 8px 0px,
+    0px 0px 2px #1949a1;
+  border: 1px solid rgb(66, 133, 244);
+  transition: 250ms;
 
-    @media (max-width: 768px) {
-        padding: 0.6rem 0.5rem;
-    }
+  @media (max-width: 768px) {
+    padding: 0.6rem 0.5rem;
+  }
 `;
 
 const FlexContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 const UserCard = styled.div`
-    display: flex;
-    width: 100%;
-    max-width: 300px;
-    height: 100px;
-    justify-content: center;
-    align-items: center;
-    margin: 5px auto;
-    
-    @media (min-width: 768px) {
-        width: 48%;
-    }
+  display: flex;
+  width: 100%;
+  max-width: 300px;
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  margin: 5px auto;
 
-    @media (min-width: 1024px) {
-        width: 32%;
-    }
+  @media (min-width: 768px) {
+    width: 48%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 32%;
+  }
 `;
 
 const UserAvatar = styled.div`
-    margin: 0 10px;
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-    background: orange;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 0;
+  margin: 0 10px;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background: orange;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
 `;
 
 const UserInfo = styled.div`
-    margin-left: 5px;
-    width: 225px;
-    font-size: 14px;
+  margin-left: 5px;
+  width: 225px;
+  font-size: 14px;
 `;
 
-const users = [
-    { name: "Kapil Zinda", email: "ZindaHaiKapil@mail.com" },
-    { name: "Rishabh Kumar", email: "rishabh@mail.com" },
-    { name: "John Doe", email: "john@mail.com" },
-    // Add more users here
-];
-
-export const UserList = () => {
-    return (
-        <FlexContainer>
-            {users.map((user, index) => (
-                <UserCard key={index}>
-                    <UserAvatar>
-                        <User2Icon style={{ height: "30px", width: "30px" }} />
-                    </UserAvatar>
-                    <UserInfo>
-                        <div>{user.name}</div>
-                        <div style={{ fontSize: "12px" }}>{user.email}</div>
-                    </UserInfo>
-                </UserCard>
-            ))}
-        </FlexContainer>
-    );
-};
-
-const Overview = () => {
-    return (
-        <>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "0 1rem" }}>
-                <h1 style={{ fontSize: "22px", textAlign: "center", marginBottom: "1rem" }}>Uchhal Organisation Management Dashboard</h1>
-                <Button variant="outlined" style={{ marginBottom: "1rem" }}>
-                    <PenIcon style={{ marginRight: "8px", width: "15px" }} /> Edit Details
-                </Button>
-            </div>
-            <Card>
-                <h2 style={{ margin: "10px 5px", fontSize: "20px", fontWeight: "500" }}>Organisation Details</h2>
-                <hr style={{ margin: "10px 0" }} />
-                <div style={{ fontSize: "18px", margin: "24px 1rem" }}>
-                    <h3>Organisation Name</h3>
-                    <p style={{ fontSize: "14px", margin: "5px 0rem", color: "#696969" }}>Uchhal Data Service</p>
-                </div>
-                <div style={{ fontSize: "18px", margin: "5px 1rem" }}>Description</div>
-                <p style={{ fontSize: "14px", margin: "5px 1rem", color: "#696969" }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolore neque possimus, doloribus dolorem nesciunt in cum blanditiis aperiam amet?</p>
-                <hr style={{ margin: "10px 0" }} />
-                <div style={{ fontSize: "18px", margin: "5px 1rem" }}>Organisation Owner</div>
-                <UserCard>
-                    <UserAvatar>
-                        <User2Icon style={{ height: "30px", width: "30px" }} />
-                    </UserAvatar>
-                    <UserInfo>
-                        <div>Kapil Zinda</div>
-                        <div style={{ fontSize: "12px" }}>Kapilzindahai@mail.com</div>
-                    </UserInfo>
-                </UserCard>
-                <div style={{ fontSize: "18px", margin: "5px 1rem" }}>Organisation Admins</div>
-                <UserList />
-            </Card>
-        </>
-    )
+interface Permission {
+  [key: string]: string;
 }
 
-export default Overview
+interface OverviewData {
+  name?: string;
+  address?: string;
+  description?: string;
+  created_ts?: number;
+  modified_ts?: number;
+  id?: string;
+  is_active?: boolean;
+  key_id?: string;
+  org?: string;
+  owner?: string[];
+  permission?: Permission;
+}
+
+interface Permission {
+  [key: string]: string; // Key is the email, value is the role
+}
+
+interface UserListProps {
+  permission: Permission;
+}
+
+export const UserList: React.FC<UserListProps> = ({ permission }) => {
+  return (
+    <FlexContainer>
+      {Object.entries(permission).map(([email, role], index) => {
+        return (
+          <UserCard key={index}>
+            <UserAvatar>
+              <User2Icon style={{ height: '30px', width: '30px' }} />
+            </UserAvatar>
+            <UserInfo>
+              <div>{role}</div> {/* Displaying the role (value) */}
+              <div style={{ fontSize: '12px' }}>{email}</div> {/* Displaying the email (key) */}
+            </UserInfo>
+          </UserCard>
+        );
+      })}
+    </FlexContainer>
+  );
+};
+
+
+// TODO: implement expiration-based token cleanup and re-login handler
+const BaseURLAuth = process.env.BaseURLAuth || '';
+
+const Overview: React.FC = () => {
+  const [overviewData, setOverviewData] = useState<OverviewData>({});
+  const bearerData = localStorage.getItem('bearerToken');
+  let jsonBearer: { value?: string } | null = null;
+  let bearerToken = 'rohan';
+
+  if (bearerData) {
+    try {
+      jsonBearer = JSON.parse(bearerData);
+      bearerToken = jsonBearer?.value || 'rohan';
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`${BaseURLAuth}/organizations/1`, {
+          headers: {
+            Authorization: `Bearer ${bearerToken}`,
+            'Content-Type': 'application/vnd.api+json',
+          },
+        });
+        setOverviewData(res.data.data.attributes);
+        console.log(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [bearerToken]);
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '0 1rem',
+        }}
+      >
+        <h1
+          style={{
+            fontSize: '22px',
+            textAlign: 'center',
+            marginBottom: '1rem',
+          }}
+        >
+          Organisation Management Dashboard
+        </h1>
+        <Button variant="outlined" style={{ marginBottom: '1rem' }}>
+          <PenIcon style={{ marginRight: '8px', width: '15px' }} /> Edit Details
+        </Button>
+      </div>
+      {overviewData.name ? (
+        <Card>
+          <h2
+            style={{ margin: '10px 5px', fontSize: '20px', fontWeight: '500' }}
+          >
+            Organisation Details
+          </h2>
+          <hr style={{ margin: '10px 0' }} />
+          <div style={{ fontSize: '18px', margin: '24px 1rem' }}>
+            <h3>Organisation Name</h3>
+            <p
+              style={{ fontSize: '14px', margin: '5px 0rem', color: '#696969' }}
+            >
+              {overviewData?.name}
+            </p>
+          </div>
+          <div style={{ fontSize: '18px', margin: '5px 1rem' }}>
+            Description
+          </div>
+          <p style={{ fontSize: '14px', margin: '5px 1rem', color: '#696969' }}>
+            {overviewData?.description}
+          </p>
+          <hr style={{ margin: '10px 0' }} />
+          <div style={{ fontSize: '18px', margin: '5px 1rem' }}>
+            Organisation Owner
+          </div>
+          <UserCard>
+            <UserAvatar>
+              <User2Icon style={{ height: '30px', width: '30px' }} />
+            </UserAvatar>
+            <UserInfo>
+              <div>{overviewData?.owner?.[0]}</div>
+              <div style={{ fontSize: '12px' }}>{overviewData?.owner?.[1]}</div>
+            </UserInfo>
+          </UserCard>
+          <div style={{ fontSize: '18px', margin: '5px 1rem' }}>
+            Organisation Admins
+          </div>
+          {overviewData?.permission && <UserList permission={overviewData.permission} />}
+        </Card>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
+};
+
+export default Overview;
