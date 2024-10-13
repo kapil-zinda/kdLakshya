88db7de0375de5facb734c170d4dfd7f0d9c5b96
Kapil Kumar <kapil.kumar@elucidata.io>
2024-10-13 06:08:45 +0530

    fixed conflicts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BaseURL : "https://sw3px4kmhm3irjwvvq5bfsoupi0wtxmj.lambda-url.ap-south-1.on.aws/",
+        BaseURL : "https://sw3px4kmhm3irjwvvq5bfsoupi0wtxmj.lambda-url.ap-south-1.on.aws/"
     }
 };
 
